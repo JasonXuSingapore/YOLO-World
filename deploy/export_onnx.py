08b5e95e50c1730f9e1ff9ccac3aacaa169122e8
JasonXuSingapore <974927201@qq.com>
2025-11-23 18:23:45 +0800

    1

--- deploy/export_onnx.py
+++ deploy/export_onnx.py
@@ -22,7 +22,7 @@ warnings.filterwarnings(action='ignore', category=ResourceWarning)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgntParser()
     parser.add_argument('config', help='Config file')
     parser.add_argument('checkpoint', help='Checkpoint file')
     parser.add_argument('--custom-text',
